6fe777d39ff0fb0f6e8d56d1cffaf22e10676dcf
killer <mwshubham@gmail.com>
2019-01-02 23:59:04 +0530

    Enabling data-binding...

--- app/src/main/java/com/example/demoarchcomp/MainActivity.kt
+++ app/src/main/java/com/example/demoarchcomp/MainActivity.kt
@@ -2,11 +2,18 @@ package com.example.demoarchcomp
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.example.demoarchcomp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+        binding.user = User("Shubham", "Agarwal")
+
+        // Specify the current activity as the lifecycle owner.
+        // it's doing something but idk
+        binding.setLifecycleOwner(this)
     }
 }
